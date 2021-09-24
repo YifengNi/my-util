09f151400be9bddca59dfddda8065d0fec4e921c
niyf <niyf@xiaopeng.com>
2021-09-24 15:01:22 +0800

    修改是否需要进行字符串转换

--- src/main/java/com/yifeng/util/controller/UtilController.java
+++ src/main/java/com/yifeng/util/controller/UtilController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,16 +27,21 @@ public class UtilController {
     @GetMapping("/stringConvert")
     @ApiOperation("字符串转换")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "camelToUnderscore", value = "驼峰转大写下划线", required = true, dataType = "String"
+            @ApiImplicitParam(name = "camelToUnderscore", value = "驼峰转大写下划线", required = false, dataType = "String"
                     , paramType = "query", defaultValue = "testWord"),
-            @ApiImplicitParam(name = "underscoreToCamel", value = "大写下划线转驼峰", required = true, dataType = "String"
+            @ApiImplicitParam(name = "underscoreToCamel", value = "大写下划线转驼峰", required = false, dataType = "String"
                     , paramType = "query", defaultValue = "TEST_WORD"),
     })
-    public Map<String, Object> queryCarConfigSeries(@RequestParam String camelToUnderscore
-            , String underscoreToCamel) {
+    public Map<String, Object> queryCarConfigSeries(@RequestParam(required = false) String camelToUnderscore
+            , @RequestParam(required = false) String underscoreToCamel) {
         Map<String, Object> data = new HashMap<>();
-        data.put(camelToUnderscore, CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, camelToUnderscore));
-        data.put(underscoreToCamel, CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, underscoreToCamel));
+
+        if (StringUtils.isNotBlank(camelToUnderscore)) {
+            data.put(camelToUnderscore, CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, camelToUnderscore));
+        }
+        if (StringUtils.isNotBlank(underscoreToCamel)) {
+            data.put(underscoreToCamel, CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, underscoreToCamel));
+        }
 
         return data;
     }
