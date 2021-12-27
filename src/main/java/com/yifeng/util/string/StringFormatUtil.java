8e29d59c2c745f691a49af8c3b0c2b032a25317b
niyf <1257594255@qq.com>
2021-12-27 17:42:06 +0800

    完善生成dto字段代码

--- src/main/java/com/yifeng/util/string/StringFormatUtil.java
+++ src/main/java/com/yifeng/util/string/StringFormatUtil.java
@@ -1,13 +1,10 @@
 package com.yifeng.util.string;
 
 import com.yifeng.util.file.FileUtil;
-import org.apache.commons.collections4.MapUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +18,9 @@ public class StringFormatUtil {
     public static final String FULL_DATE_FORMAT_STRING = "yyyyMMddHHmmssSSS";
 
     public static void main(String[] args) {
+        String fileName = "E:\\文档\\工作\\数据库select语句字段.txt";
+        generateCodeFromDbFieldTxtV2(fileName);
+
         // String fileName = "E:\\文档\\工作\\数据库select语句字段.txt";
         // generateCodeFromDbFieldTxt(fileName);
 
@@ -33,27 +33,31 @@ public class StringFormatUtil {
         // String format = LocalDateTime.now().format(DateTimeFormatter.ofPattern(FULL_DATE_FORMAT_STRING));
         // System.out.println("format = " + format);
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("aa", 123);
-        map.put("bb", 34L);
-        map.put("cc", true);
-        map.put("dd", "56");
-
-        String aa = MapUtils.getString(map, "aa");
-        String bb = MapUtils.getString(map, "bb");
-        String cc = MapUtils.getString(map, "cc");
-        Integer dd = MapUtils.getInteger(map, "dd");
-        Long aa2 = MapUtils.getLong(map, "aa");
-        Integer bb2 = MapUtils.getInteger(map, "bb");
-
-        System.out.println("aa = " + aa);
-        System.out.println("bb = " + bb);
-        System.out.println("cc = " + cc);
-        System.out.println("dd = " + dd);
-        System.out.println("aa2 = " + aa2);
-        System.out.println("bb2 = " + bb2);
+        // Map<String, Object> map = new HashMap<>();
+        // map.put("aa", 123);
+        // map.put("bb", 34L);
+        // map.put("cc", true);
+        // map.put("dd", "56");
+        //
+        // String aa = MapUtils.getString(map, "aa");
+        // String bb = MapUtils.getString(map, "bb");
+        // String cc = MapUtils.getString(map, "cc");
+        // Integer dd = MapUtils.getInteger(map, "dd");
+        // Long aa2 = MapUtils.getLong(map, "aa");
+        // Integer bb2 = MapUtils.getInteger(map, "bb");
+        //
+        // System.out.println("aa = " + aa);
+        // System.out.println("bb = " + bb);
+        // System.out.println("cc = " + cc);
+        // System.out.println("dd = " + dd);
+        // System.out.println("aa2 = " + aa2);
+        // System.out.println("bb2 = " + bb2);
     }
 
+    /**
+     * 根据api文档（从Excel复制出来，字段间使用制表符/空格分隔）生成dto字段代码
+     * @param fileName
+     */
     public static void generateCodeFromApiTxt(String fileName) {
         List<String> stringList = new ArrayList<>();
         for (String s : FileUtil.readToStringList(fileName)) {
@@ -82,6 +86,10 @@ public class StringFormatUtil {
         FileUtil.writeToFile(stringList, FileUtil.getWriteFileName(fileName));
     }
 
+    /**
+     * 根据数据库sql查询字段列表生成dto字段代码
+     * @param fileName
+     */
     public static void generateCodeFromDbFieldTxt(String fileName) {
         List<String> stringList = new ArrayList<>();
         String regex = "^\\w+$";
@@ -136,6 +144,111 @@ public class StringFormatUtil {
                     throw new RuntimeException("字符串不包含合法字段名");
                 }
 
+            } catch (Exception e) {
+                System.out.println("报错：-->" + global + "<--> " + e.getMessage());
+            }
+        }
+
+        FileUtil.writeToFile(stringList, FileUtil.getWriteFileName(fileName));
+    }
+
+    /**
+     * 根据数据库sql查询字段列表生成dto字段代码（包含注释）
+     * generateCodeFromDbFieldTxt的升级版
+     * @param fileName
+     */
+    public static void generateCodeFromDbFieldTxtV2(String fileName) {
+        List<String> stringList = new ArrayList<>();
+        // 字段
+        String fieldRegex = "^\\w+$";
+        Pattern fieldPattern = Pattern.compile(fieldRegex);// 匹配的模式
+        // 注释
+        String remarkRegex = "^/\\*+\\s*(.+)\\s*\\*+/$";
+        Pattern remarkPattern = Pattern.compile(remarkRegex);// 匹配的模式
+
+        String defaultRemark = "请添加备注";
+
+        int idx = -1;
+        for (String s : FileUtil.readToStringList(fileName, true)) {
+            String global = s;
+            boolean isSuccessful = false;
+
+            try {
+                // 根据逗号切割
+                String[] splitByComma = s.split(",");
+
+                // 当前行只是注释行，则有可能是上一个字段的注释，比如下面的情况
+                // sto.FIRSTSHIPPING_ORDER_TIME,
+                // /*发运指令时间*/
+                if (splitByComma.length == 1 && idx >= 0) {
+                    s = splitByComma[0].trim();
+                    Matcher remarkMatcher = remarkPattern.matcher(s);
+                    if (remarkMatcher.find()) {
+                        String lastStr = stringList.get(idx).replaceFirst(defaultRemark, remarkMatcher.group(1));
+                        stringList.set(idx, lastStr);
+                        continue;
+                    }
+                }
+
+                boolean isRemarkNotFound = true;
+                String remark = defaultRemark;
+                // 从后往前遍历
+                for (int i = splitByComma.length - 1; i >= 0; i--) {
+                    s = splitByComma[i].trim();
+                    // 判断是否是备注，是就获取该值，然后遍历下一个
+                    if (isRemarkNotFound) {
+                        Matcher remarkMatcher = remarkPattern.matcher(s);
+                        if (remarkMatcher.find()) {
+                            remark = remarkMatcher.group(1);
+                            isRemarkNotFound = false;
+                            continue;
+                        }
+                    }
+
+                    // 根据空格切割
+                    String[] splitBySpace = s.split("\\s");
+
+                    for (int j = splitBySpace.length - 1; j >= 0; j--) {
+                        s = splitBySpace[j].trim();
+                        // 根据点号切割
+                        String[] splitByPoint = s.split("\\.");
+
+                        for (int k = splitByPoint.length - 1; k >= 0; k--) {
+                            s = splitByPoint[k].trim();
+                            s = StringUtil.underscoreToCamel(s);
+
+                            Matcher fieldMatcher = fieldPattern.matcher(s);
+                            if (fieldMatcher.matches()) {
+
+                                // @ApiModelProperty("收车价格")
+                                // @JsonProperty("SECOND_RECEIVE_PRICE")
+                                // private String secondReceivePrice;
+                                StringBuilder sb = new StringBuilder();
+                                sb.append("\t@ApiModelProperty(\"").append(remark).append("\")\n\t")
+                                        // .append("@JsonProperty(\"").append(ss[0]).append("\")\n\t")
+                                        .append("private ").append("String").append(" ").append(s).append(";\n");
+
+                                stringList.add(sb.toString());
+                                isSuccessful = true;
+                                break;
+                            }
+                        }
+
+                        if (isSuccessful) {
+                            break;
+                        }
+                    }
+
+                    if (isSuccessful) {
+                        break;
+                    }
+                }
+
+                if (!isSuccessful) {
+                    throw new RuntimeException("字符串不包含合法字段名");
+                }
+
+                idx++;
             } catch (Exception e) {
                 System.out.println("报错：-->" + global + "<--> " + e.getMessage());
             }
